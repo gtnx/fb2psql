528c2c96432176999a5b495bea9416faf167bf28
Guillaume Thomas <guillaume.thomas642@gmail.com>
2015-06-19 15:40:51 +0200

    New main based on fdb

--- main.py
+++ main.py
@@ -1,77 +1,133 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import FieldProperty
-import TableProperty
-import MoveData
-import MoveGenerators
-import MoveTriggers
+import time
+import fdb
+import psycopg2
+import argparse
+import logging
+import re
 
-from Connect import openFB, openPSQL, createTable
+parser = argparse.ArgumentParser(description='fb2psql')
+parser.add_argument('--fb-host', dest='fb_host', default='localhost')
+parser.add_argument('--fb-database', dest='fb_database', default='')
+parser.add_argument('--fb-user', dest='fb_user', default='sysdba')
+parser.add_argument('--fb-password', dest='fb_password', default='masterkey')
+
+parser.add_argument('--pq-host', dest='pq_host', default='localhost')
+parser.add_argument('--pq-database', dest='pq_database', default='postgres')
+parser.add_argument('--pq-user', dest='pq_user', default='postgres')
+parser.add_argument('--pq-password', dest='pq_password', default='postgres')
+
+args = parser.parse_args()
+
+logger = logging.getLogger('fb2psql')
+formatter = logging.Formatter(
+    fmt='%(asctime)s.%(msecs)03d, %(name)s, %(levelname)s, %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
 
 
-conFB, curFB = openFB()
-conPSQL, curPSQL = openPSQL()
+def run_query(cursor, qry):
+    start = time.time()
+    qry = re.sub('^([ \n]+)', '', qry)
+    qry = re.sub('[ \n]+', ' ', qry)
+    cursor.execute(qry)
+    try:
+        retval = cursor.fetchall()
+    except psycopg2.ProgrammingError:
+        retval = []
+    logger.info('(%.3fs) %s' % (time.time() - start, qry.replace('\n', ' ')[:1000]))
+    return retval
 
-try:
-    curFB.execute("""
-    SELECT R.RDB$RELATION_NAME AS "TABLENAME",
-      F.RDB$FIELD_TYPE AS "TYPE",
-      F.RDB$CHARACTER_LENGTH AS "LENGTH",
-      F.RDB$FIELD_SCALE AS "SCALE",
-      F.RDB$FIELD_SUB_TYPE AS "SUB_TYPE",
-      F.RDB$FIELD_PRECISION AS "PRECISION",
-      R.RDB$FIELD_NAME AS "NAME",
-      R.RDB$DEFAULT_SOURCE AS "DEFAULT_VALUE",
-      F.RDB$CHARACTER_SET_ID AS "CHARACTER_SET_ID",
-      R.RDB$NULL_FLAG
-FROM RDB$FIELDS F,
-               RDB$RELATION_FIELDS R
-WHERE F.RDB$FIELD_NAME = R.RDB$FIELD_SOURCE
-  AND R.RDB$SYSTEM_FLAG = 0
-ORDER BY R.RDB$RELATION_NAME,
-             R.RDB$FIELD_POSITION
-    """)
-    rows = curFB.fetchall()
-    relationName = None
-    table = TableProperty.TableProperty()
-    print u"all fields count: ", len(rows)
-    count = 0
-    for row in rows:
-        if relationName is not None and relationName != unicode(row[0].strip()):
-            createTable(table, relationName, curFB, curPSQL)
-            conPSQL.commit()
-            MoveData.MoveData(table).run()
-            table = TableProperty.TableProperty()
-            del table.fields[:]
-            del table.fieldsProp[:]
-            del table.primaryKey[:]
 
-        relationName = unicode(row[0].strip())
-        fp = FieldProperty.FieldProperty()
-        fp.type = row[1]
-        fp.length = row[2]
-        fp.scale = row[3]
-        fp.subType = row[4]
-        fp.precision = row[5]
-        fp.name = row[6].strip()
-        fp.defaultValue = row[7]
-        if row[8] is not None:
-            fp.charset = row[8]
-        fp.nullFlag = row[9]
-        table.fields.append(unicode(fp))
-        table.fieldsProp.append(fp)
-        count += 1
-        if count % 1000 == 0:
-            print "using ", count, "fields, remain: ", (len(rows) - count)
+def drop_table(cursor, table):
+    return run_query(cursor, 'drop table if exists %(table)s' % locals())
 
-    createTable(table, relationName, curFB, curPSQL)
-    conPSQL.commit()
-    MoveData.MoveData(table).run()
-    MoveTriggers.MoveTriggers().run()
-finally:
-    curFB.close()
-    curPSQL.close()
-    conFB.close()
-    conPSQL.close()
-MoveGenerators.MoveGenerators().run()
+
+def null_str(nullable):
+    return 'NULL' if nullable else 'NOT NULL'
+
+
+def default_str(cursor, default):
+    if default is not None:
+        # default = "'%s'" % default if isinstance(default, (str, unicode)) else default
+        return 'DEFAULT %s' % default
+    return ''
+
+
+def unique_str(unique):
+    return 'UNIQUE ' if unique else ''
+
+
+def create_column(cursor, column):
+    datatype = column.datatype
+    if datatype.startswith('BLOB '):
+        datatype = 'bytea'
+    return '%s %s %s %s' % (
+        column.name,
+        datatype,
+        null_str(column.isnullable()),
+        default_str(pq_cursor, column.default)
+    )
+
+
+def create_constraint(cursor, constraint):
+    if constraint.ispkey():
+        return 'PRIMARY KEY (%s)' % ','.join([s.name for s in constraint.index.segments])
+
+
+def consider_constraint(constraint):
+    return constraint.ispkey()
+
+
+def insert_data(cursor, rows, it):
+        fields = len(rows) // it
+        template = '(' + ', '.join(['%s'] * fields) + ')'
+        template = ', '.join([template] * it)
+        qry = 'INSERT INTO %s values %s' % (table.name, template)
+        cursor.execute(qry, rows)
+        table_name = table.name
+        logger.info('Saving %(it)s elements in %(table_name)s' % locals())
+
+
+def copy_data(table, fb_cursor, pq_cursor, bulk_nb=1000):
+    values = []
+    it = 0
+    for row in run_query(fb_cursor, 'SELECT * FROM %s' % table.name):
+        values.extend(row)
+        it += 1
+        if it % bulk_nb == 0:
+            insert_data(pq_cursor, values, it)
+            values = []
+            it = 0
+    if it:
+        insert_data(pq_cursor, values, it)
+
+if __name__ == '__main__':
+    fb_args = dict([(k[3:], v) for k, v in args.__dict__.items() if k.startswith('fb_')])
+    fb_conn = fdb.connect(**fb_args)
+    fb_cursor = fb_conn.cursor()
+    pq_args = dict([(k[3:], v) for k, v in args.__dict__.items() if k.startswith('pq_')])
+    pq_conn = psycopg2.connect(**pq_args)
+    pq_cursor = pq_conn.cursor()
+
+    for table in fb_conn.schema.tables:
+        drop_table(pq_cursor, table.name)
+        columns = [create_column(pq_cursor, column) for column in table.columns]
+        constraints = [create_constraint(pq_cursor, constraint) for constraint in table.constraints if consider_constraint(constraint)]
+        qry = """
+            create table %s
+            (%s)
+        """ % (table.name, ', '.join(columns + constraints))
+        run_query(pq_cursor, qry)
+        for index in table.indices:
+            if not index.constraint or not index.constraint.ispkey():
+                run_query(pq_cursor, 'create %s index %s on %s(%s)' % (unique_str(index.isunique()), index.name, table.name, ', '.join([c.name for c in index.segments])))
+        copy_data(table, fb_cursor, pq_cursor)
+
+    pq_conn.commit()
